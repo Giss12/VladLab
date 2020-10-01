66015d518ec5c58726aabf835575c831694b773b
Giss12 <36027903+Giss12@users.noreply.github.com>
2020-10-01 18:16:16 +0300

    Add files via upload

---
+++ Pool.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class Pool : MonoBehaviour
+{
+    public static Pool Instance = null;
+    [System.Serializable]
+    public class PoolObject
+    {
+        public GameObject prefab;
+        public int amount;
+    }
+    public List<PoolObject> poolObjects;
+    public List<GameObject> objectsOnScene;
+    [SerializeField][Header("Координаты родительского объекта")] Transform spawner;
+
+    private void Start()
+    {
+        if(Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            Destroy(this.gameObject);
+        }
+        foreach (PoolObject item in poolObjects)
+        {
+            for (int i = 0; i < item.amount; i++)
+            {
+                if (item.prefab.tag == "Bullet")
+                {
+                    GameObject objBullet = Instantiate(item.prefab, spawner);
+                    objectsOnScene.Add(objBullet);
+                    objBullet.SetActive(false);
+                }
+            }
+        }
+    }
+    public GameObject Get(string tag)
+    {
+         foreach (GameObject item in objectsOnScene)
+         {
+             if(item.CompareTag(tag) && !item.activeInHierarchy)
+             {
+                 item.transform.position = new Vector3(Input.mousePosition.x, 0, 0);
+                 item.SetActive(true);
+                 return item;
+             }
+         }
+        Debug.Log("Пуль нету");
+        return null;
+    }
+}
---
+++ Shoot.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shoot : MonoBehaviour
+{
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            //Debug.Log(Camera.main.ScreenToWorldPoint(Input.mousePosition.x));
+            Pool.Instance.Get("Bullet");
+        }
+    }
+}
